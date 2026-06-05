b081d68a2be66248753a7e3deb4c2d668f225458
kalvacherlapranay <kalvacherlapranay@gmail.com>
2026-06-05 14:43:19 +0530

    Add files via upload

---
+++ fibonaucci.java
@@ -0,0 +1,18 @@
+class fibonaucci{
+public static int fib(int n){
+if(n == 0 || n ==1)
+{
+return n;
+}
+else
+{
+return fib(n-2) + fib(n-1);
+}
+}
+public static void main(String args[])
+{
+System.out.println(fib(6));
+}
+}
+
+ 
